69555c56283912c7bd97e981c3488511ca1efad7
Anastasia Zef <zeff1983@gmail.com>
2025-03-29 17:14:44 +0300

    Add files via upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+  	return false;
+  }
+
+  return arr1.every((value, index) => {
+  	console.log(value, arr2[index]);
+  	return value === arr2[index];
+  });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let filter = users.filter(user => user.gender === gender);
+	if (filter.length === 0) {
+		return 0;
+	}
+  let total = filter.reduce((sum, user) => sum + user.age, 0);
+  let avg = total / filter.length;
+  return avg;
 }
