6f5d5379d1af60f13bd83d3ac48230b502e808ea
Anastasia Zef <zeff1983@gmail.com>
2025-03-29 19:00:28 +0300

    Add files via upload

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+
+  this.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
+
+}
+Student.prototype.addMarks = function(...marksToAdd) {
+  if (!this.marks) {
+    console.log("Студент отчислен.");
+    return;
+  }
+
+  if (!!marksToAdd.length) {
+    this.marks.push(...marksToAdd);
+  }
+  return;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.getAverage = function() {
+  if (!this.marks || !this.marks.length) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, curr) => acc + curr);
+  return sum / this.marks.length;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
 }
